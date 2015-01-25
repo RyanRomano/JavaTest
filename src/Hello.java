760415663f94fb8df32cfdd35c51f60a0d885be2
Ryan Romano <ryanaromano@gmail.com>
2015-01-24 19:16:03 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,29 @@
+
+public class Hello {
+
+	public static void main(String[] args)
+	{
+		howBadDidIDo(10);
+	}
+	
+	public static void howBadDidIDo(double number) {
+		if (number > 4)
+		{
+			String toPrint = "I fucked up ";
+			toPrint = toPrint + squareNumber(number);
+			toPrint = toPrint + "X";
+			System.out.println(toPrint);
+		}
+		else
+		{
+			System.out.println("I did good.");
+		}
+	}
+	
+	public static double squareNumber(double number)
+	{
+		double elephant = number * number;
+		return elephant;
+	}
+	
+}
