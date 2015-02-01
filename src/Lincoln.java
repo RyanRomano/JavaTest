41a2b58b53d8c38189b9f0468c5d52859437e765
Ryan Romano <ryanaromano@gmail.com>
2015-02-01 16:17:10 -0500

    Adding homework 1 files

---
+++ src/Lincoln.java
@@ -0,0 +1,10 @@
+
+public class Lincoln {
+
+	public static void main(String[] args) {
+		System.out.println("A quote by Abraham Lincoln:");
+		System.out.println("\"Whatever you are, be a good one.\"");
+
+	}
+
+}
---
+++ src/SquareaPrinter.java
@@ -0,0 +1,10 @@
+
+public class SquareaPrinter {
+	public static void main (String[] args) {
+		int squareSide = 5; //This number can be anything!!
+		int perimeter = squareSide * 4;
+		int area = squareSide * squareSide;
+		System.out.println("The square's area is " + area + ".");
+		System.out.println("The square's perimeter is " + perimeter + ".");		
+	}
+}
